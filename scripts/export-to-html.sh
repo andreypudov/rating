a3b9f2560a0069e240afbd05ee0f3b46ee3e00ff
Andrey Pudov <mail@andreypudov.com>
2024-06-18 15:00:37 +0200

    2024-06-18 13:00:37Z

--- scripts/export-to-html.sh
+++ scripts/export-to-html.sh
@@ -2,7 +2,14 @@
 
 cd "$(dirname -- "$(which -- "$0" 2>/dev/null || realpath -- "$0")")" || exit 1
 
-for file in *.org; do
+rating_dir="$HOME/rating"
+
+if [ ! -d "$rating_dir" ]; then
+  echo "Directory of the Rating repository is not found."
+  exit 1
+fi
+
+for file in "$rating_dir"/*.org; do
   echo "Processing $file"
   emacs \
     --batch \
@@ -11,5 +18,6 @@ for file in *.org; do
     --funcall org-html-export-to-html
 done
 
-mv README.html index.html
-cp index.html 404.html
+mv "$rating_dir/"*.html "$rating_dir/docs/"
+mv "$rating_dir/docs/README.html" "$rating_dir/docs/index.html"
+cp "$rating_dir/docs/index.html" "$rating_dir/docs/404.html"
--- scripts/sync-database.sh
+++ scripts/sync-database.sh
@@ -3,20 +3,26 @@
 cd "$(dirname -- "$(which -- "$0" 2>/dev/null || realpath -- "$0")")" || exit 1
 
 agenda_dir="$HOME/agenda"
+rating_dir="$HOME/rating"
 
 if [ ! -d "$agenda_dir" ]; then
   echo "Directory of the Agenda repository is not found."
   exit 1
 fi
 
-cp "$agenda_dir/books.org" \
+if [ ! -d "$rating_dir" ]; then
+  echo "Directory of the Rating repository is not found."
+  exit 1
+fi
+
+
+
+if ! cp "$agenda_dir/books.org" \
   "$agenda_dir/cooking.org" \
   "$agenda_dir/games.org" \
   "$agenda_dir/movies.org" \
   "$agenda_dir/music.org" \
-  .
-
-if [ "$?" -ne 0 ]; then
+  "$rating_dir" 1>/dev/null; then
   echo "Failed to copy files."
   exit 1
 fi
